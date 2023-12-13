26ac0b1078e47d680c343a0b208d5bbf821b1149
Jeremy Kescher <jeremy@kescher.at>
2023-12-13 01:10:31 +0100

    Day one, part one

---
+++ DayOne/DayOne.PartOne/Program.cs
@@ -0,0 +1,27 @@
+﻿var line = await Console.In.ReadLineAsync();
+var sum = 0;
+while (!string.IsNullOrEmpty(line))
+{
+    var lineTask = Console.In.ReadLineAsync();
+    var lineArr = line.ToCharArray();
+    var firstDigit = 10;
+    foreach (var c in lineArr)
+    {
+        if (!char.IsDigit(c)) continue;
+
+        firstDigit = c - '0';
+        break;
+    }
+
+    for (var i = lineArr.Length - 1; i >= 0; i--)
+    {
+        if (!char.IsDigit(lineArr[i])) continue;
+
+        sum += int.Parse($"{firstDigit}{lineArr[i] - '0'}");
+        break;
+    }
+
+    line = await lineTask;
+}
+
+Console.WriteLine(sum);
---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2023 Solutions
+
+C#, .NET 8. Did so because that's what I currently use the most due to paid work.
+
+I am too lazy for a proper license file, so: Code provided as-is. I cannot be held liable for any damage caused by this code. No warranty or guarantee that everything works.
