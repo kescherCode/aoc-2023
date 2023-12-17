0211bbdcaddad5dab0a727cfa6d0cd713a0c1eb2
Jeremy Kescher <jeremy@kescher.at>
2023-12-17 14:26:05 +0100

    Day 08, Part 1 (this could not have been more easy)

---
+++ Day08/Day08.Part1/Program.cs
@@ -0,0 +1,28 @@
+﻿var directions = Console.ReadLine().AsSpan();
+var directionsLength = directions.Length;
+var places = new Dictionary<string, (string Left, string Right)>();
+
+_ = Console.ReadLine();
+while (Console.ReadLine().AsSpan() is { IsEmpty: false } line)
+{
+    var place = line[..3];
+    var left = line.Slice(7, 3);
+    var right = line.Slice(12, 3);
+    places[new(place)] = (new(left), new(right));
+}
+
+const string goal = "ZZZ";
+var currentLocation = "AAA";
+var iterations = 0;
+do
+{
+    foreach (var direction in directions)
+    {
+        var next = places[currentLocation];
+        currentLocation = direction == 'L' ? next.Left : next.Right;
+    }
+
+    iterations++;
+} while (currentLocation != goal);
+
+Console.WriteLine(directionsLength * iterations);
